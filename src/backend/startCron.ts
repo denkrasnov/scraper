a1dd2064d73416c201e32886242978f6d2e99dc3
Denis Krasnov <dkrasnov@travix.com>
2020-03-22 11:37:51 +0100

    Disable console

--- src/backend/startCron.ts
+++ src/backend/startCron.ts
@@ -3,6 +3,7 @@ import cron from "node-cron";
 // TODO: Configure cron to start all scrappers every 2:00 pm
 const startCron = () =>
   cron.schedule("* * * * *", () => {
+    // eslint-disable-next-line no-console
     console.log("running a task every minute");
   });
 
