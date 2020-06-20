ae6fc366bd3168e1ad8bf8c983e277c27bf8a838
Denis Krasnov <dkrasnov@travix.com>
2020-06-20 13:03:54 +0200

    Test CD

--- src/frontend/components/Header/Header.tsx
+++ src/frontend/components/Header/Header.tsx
@@ -7,7 +7,7 @@ const Header: FC = () => {
   return (
     <header>
       <Box alignItems="center" height="60px" justifyContent="center">
-        <Text color="MAIN_RED" fontSize="fs16" fontWeight="fw700">
+        <Text color="BODY_TEXT" fontSize="fs16" fontWeight="fw700">
           compare
         </Text>
       </Box>
