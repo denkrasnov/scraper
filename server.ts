907b0a6715efe59771dd60dc68d3376396c49df9
Denis Krasnov <dkrasnov@travix.com>
2020-06-28 20:22:58 +0200

    chore(e2e): Provide env variables

--- server.ts
+++ server.ts
@@ -13,7 +13,9 @@ const app: Application = express();
 
 process.stdout.write(`
  ${chalk.bgHex("#224dff").white("--- Compare md ---")}
- The server is available on ${chalk.hex("#f7c132")(`${process.env.PORT}`)}
+ The server is available on ${chalk.hex("#f7c132")(`${process.env.PORT}`)} on ${
+  process.env.NODE_ENV
+} env
 \n`);
 
 app.use(bodyParser.json());
