f08f0c05f80a27a9ca03dddd10b55869346436de
Denis Krasnov <dkrasnov@travix.com>
2021-01-10 17:45:43 +0100

    Enable scrape on cron

--- src/backend/scrapers/startCron.ts
+++ src/backend/scrapers/startCron.ts
@@ -1,12 +1,12 @@
 import cron from "node-cron";
 import chalk from "chalk";
 
-// import scrape from "./scrape";
+import scrape from "./scrape";
 
 // start the job at 0 seconds, 0 minutes every 3rd hour'
 const startCron = () =>
   cron.schedule("0 0 */3 * * *", () => {
-    // scrape();
+    scrape();
     console.log(chalk.bold.green("Cron job finished 🕑"));
   });
 
